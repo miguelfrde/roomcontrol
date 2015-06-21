da4b9881324e3d8d5c653450ad4522e0f636cd16
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-06-21 11:59:44 +0200

    Move sounds and light programs to the controller

--- jssrc/alarm/AlarmCtrl.js
+++ jssrc/alarm/AlarmCtrl.js
@@ -1,8 +1,29 @@
 'use strict';
 
+// TODO: get sounds and lightprograms available from the RaspberryPi
+const sounds = [
+  'Extreme wake up',
+  'Releaxed wake up',
+  'Crazy wake up',
+  'Beautiful morning',
+  'Cold morning'
+];
+
+const lightprograms = [
+  'Morning lights',
+  'Colorful',
+  'Orange morning',
+  'Purple morning'
+];
+
 export class AlarmCtrl {
   constructor($scope) {
     const self = this;
+
+    $scope.sounds = sounds;
+    $scope.lightprograms = lightprograms;
+    $scope.lightprogram = $scope.lightprograms[1];
+    $scope.sound = $scope.sounds[2];
 
     $scope.tpslots = {epochTime: 30600, format: 12, step: 5};
     $scope.time = self.epochToTime(30600)
