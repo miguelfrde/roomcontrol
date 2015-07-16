fa902edd89184cd40fb0c2a543403d20301893ac
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-16 15:38:34 +0200

    Add about information to the settings view

--- jssrc/services/LocalStorage.js
+++ jssrc/services/LocalStorage.js
@@ -13,12 +13,22 @@ export class LocalStorage {
     this.$window.localStorage[key] = value;
   }
 
-  setObject(key, value) {
+  _setObject(key, value) {
     this.$window.localStorage[key] = JSON.stringify(value);
   }
 
   getObject(key) {
     return JSON.parse(this.$window.localStorage[key] || '{}');
+  }
+
+  getObjectProperty(objectKey, propKey) {
+    return this.getObject(objectKey)[propKey];
+  }
+
+  setObjectProperty(objectKey, propKey, value) {
+    const obj = this.getObject(objectKey);
+    obj[propKey] = value;
+    this._setObject(objectKey, obj);
   }
 }
 
