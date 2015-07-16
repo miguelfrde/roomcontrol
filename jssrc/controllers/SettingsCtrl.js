e844ba4d86288e1b45d6155b82722133df4fe74f
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-16 16:37:17 +0200

    Save settings to local storage

--- jssrc/controllers/SettingsCtrl.js
+++ jssrc/controllers/SettingsCtrl.js
@@ -7,6 +7,10 @@ export class SettingsCtrl {
     $scope.settings = LocalStorage.getObject('settings');
 
     $scope.save = function() { 
+      LocalStorage.setObject('settings', $scope.settings);
+      // TODO: if there was a change to the server IP, redirect to login
+      //       to verify/setup the spotify details
+      // TODO: save settings also in the server
     };
   }
 }
