ef9420ac062ca62f0ceb01ea8c51c8fa6f393ea1
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-07-17 23:58:23 +0200

    Add next/previous/play/pause buttons

--- jssrc/controllers/MusicCtrl.js
+++ jssrc/controllers/MusicCtrl.js
@@ -8,6 +8,17 @@ export class MusicCtrl {
     }).then(function(modal) {
       $scope.modal = modal;
     });
+   
+    $scope.track = Music.currentTrack;
+    $scope.currPlaylist = Music.playlist;
+    $scope.playlists = Music.playlists;
+    $scope.playStatus = true;
+
+    $scope.playPause = function() {
+      $scope.playStatus = !$scope.playStatus;
+    };
+
+    // Change playlist modal controller functions
 
     $scope.openModal = function() {
       $scope.modal.show();
@@ -16,10 +27,6 @@ export class MusicCtrl {
     $scope.closeModal = function() {
       $scope.modal.hide();
     };
-   
-    $scope.track = Music.currentTrack;
-    $scope.currPlaylist = Music.playlist;
-    $scope.playlists = Music.playlists;
 
     $scope.changePlaylist = function(newid) {
       Music.playlist = newid;
