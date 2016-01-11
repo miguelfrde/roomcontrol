efe384f30a3a24a0f8a2a82245c67b098380ab65
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2016-01-10 19:55:39 -0600

    Add music page, cordova plugins

--- app/pages/music/music.js
+++ app/pages/music/music.js
@@ -1,11 +1,47 @@
-import { NavController, Page } from 'ionic/ionic';
+import { NavController, Page, Modal, ViewController } from 'ionic/ionic';
 
 
 @Page({
   templateUrl: 'build/pages/music/music.html'
 })
 export class MusicPage {
+  song: Object {
+    name: 'Design Music',
+    artist: 'Sven Väth',
+    albumCoverUrl: 'http://geo.static.traxsource.com/files/images/356840_large.jpg'
+  };
+
   constructor(nav: NavController) {
     this.nav = nav;
+  }
+
+  openPlaylistModal() {
+    const playlistModal = Modal.create(PlaylistModal)
+    this.nav.present(playlistModal)
+  }
+}
+
+
+@Page({
+  templateUrl: 'build/pages/music/playlistModal.html'
+})
+class PlaylistModal {
+  playlists: Object[] = [
+    { name: 'Electroswingin around' },
+    { name: 'Techno' },
+    { name: 'Crazy' },
+    { name: 'Swedish Erasmus Parties' }];
+
+  constructor(viewCtrl: ViewController) {
+    this.viewCtrl = viewCtrl;
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  choosePlaylist(playlist) {
+    console.log('Choose playlist: ', playlist.name);
+    this.dismiss();
   }
 }
